f8509d1a24f0b8f491bd45352ec11d95b082373c
tjwldnjs123 <tjwldnjs12345@naver.com>
2023-05-30 15:37:23 +0900

    Feat: NewMeetupForm 파일 생성

--- components/meetups/NewMeetupForm.js
+++ components/meetups/NewMeetupForm.js
@@ -1,15 +1,18 @@
-import { TextField } from "@mui/material";
+import { Button, Card, TextField } from "@mui/material";
 
 function NewMeetupForm() {
   return (
-    <div className=" flex flex-row justify-center h-[450px]">
-      <div className="w-[80%] pt-10 flex flex-col justify-around ">
-        <TextField label="Meetup Title" fullWidth />
-        <TextField label="Meetup Image" fullWidth />
-        <TextField label="Address" fullWidth />
-        <TextField label="Description" multiline fullWidth rows={5} />
+    <Card className="w-[90%] m-auto mt-20 flex flex-col">
+      <div className=" flex flex-row justify-center h-[450px]">
+        <div className="w-[80%] p-10 flex flex-col justify-around ">
+          <TextField label="Meetup Title" fullWidth />
+          <TextField label="Meetup Image" fullWidth />
+          <TextField label="Address" fullWidth />
+          <TextField label="Description" multiline fullWidth rows={5} />
+        </div>
       </div>
-    </div>
+      <Button className="mb-5 w-30 m-auto">Add Meetup</Button>
+    </Card>
   );
 }
 
